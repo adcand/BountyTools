eb1e2a8b3754402cb0349356a9457296ad939ddd
Adriano <66135261+adcand@users.noreply.github.com>
2022-10-30 13:40:16 +0000

    added meg to the list

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/ffuf/ffuf@latest
 go install -v github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/tomnomnom/meg@latest
